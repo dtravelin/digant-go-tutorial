6d00791c2137186d0bf9445b57732c3409b97a9b
digant_t <digant.tyagi@ravelin.com>
2022-06-17 09:15:33 +0100

    fix test

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,9 +1,11 @@
 package main
 
+import "testing"
+
 func TestNewDeck(t *testing.T) {
-	d := NewDeck()
+	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Expected deck length of 16 but got", len(d))
+		t.Errorf("Expected deck length of 16 but got %v", len(d))
 	}
 }
